aed0321b44b95eb6d264372c8d26e22dd236d775
Rodrigo Leon <rodrigoleonmdp@gmail.com>
2025-06-04 10:02:18 -0300

    Substack: basic styles

--- components/shared/substack-feed.tsx
+++ components/shared/substack-feed.tsx
@@ -1,6 +1,65 @@
+'use client';
+
 import Script from 'next/script';
+import { useEffect } from 'react';
 
 export default function SubstackFeed() {
+  // useEffect(() => {
+  //   const intervalId = setInterval(() => {
+  //     const container = document.getElementById('substack-feed-embed');
+  //     console.log({ container });
+
+  //     if (!container) return;
+
+  //     clearInterval(intervalId); // ya lo encontró, no seguimos buscando
+
+  //     const handleClick = (event: Event) => {
+  //       const target = event.target as HTMLElement;
+  //       const anchor = target.closest('a') as HTMLAnchorElement;
+
+  //       if (anchor && anchor.href.includes('substack.com')) {
+  //         console.log('Clicking...');
+
+  //         // if (typeof window.gtag === 'function') {
+  //         //   window.gtag('event', 'click_substack_link', {
+  //         //     event_category: 'engagement',
+  //         //     event_label: anchor.href,
+  //         //   });
+  //         // }
+  //       }
+  //     };
+
+  //     const addListeners = () => {
+  //       const links = container.querySelectorAll('a[href*="substack.com"]');
+  //       links.forEach((link) => {
+  //         link.removeEventListener('click', handleClick); // por si ya estaba
+  //         link.addEventListener('click', handleClick);
+  //       });
+  //     };
+
+  //     const observer = new MutationObserver(() => {
+  //       addListeners();
+  //     });
+
+  //     observer.observe(container, { childList: true, subtree: true });
+
+  //     // Por si el widget ya está cargado
+  //     addListeners();
+
+  //     // Limpieza al desmontar
+  //     return () => {
+  //       observer.disconnect();
+  //       const links = container.querySelectorAll('a[href*="substack.com"]');
+  //       links.forEach((link) => {
+  //         link.removeEventListener('click', handleClick);
+  //       });
+  //     };
+  //   }, 1000); // intenta cada 500ms
+
+  //   // timeout de seguridad tras 10s
+  //   setTimeout(() => clearInterval(intervalId), 10000);
+  // }, []);
+
   return (
     <section className="mx-auto max-w-7xl py-20">
       <h1>substack feed example</h1>
@@ -11,8 +70,8 @@ export default function SubstackFeed() {
         {` 
           window.SubstackFeedWidget = {
             substackUrl: "centerforhumanetechnology.substack.com",
-            posts: 4,
-            filter: "community"
+            posts: 12,
+            filter: "new"
           };
         `}
       </Script>
